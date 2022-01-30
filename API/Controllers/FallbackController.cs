558193b82d5a91ae7cf5416bf4923e6bf7569c71
Justin Htaung <jhtaung29@gmail.com>
2022-01-29 18:02:40 -0800

    section 19 - pre switch to postgres

---
+++ API/Controllers/FallbackController.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public ActionResult Index()
+        {
+            return PhysicalFile(
+                Path.Combine(
+                    Directory.GetCurrentDirectory(), 
+                    "wwwroot", 
+                    "index.html"), 
+                "text/HTML");
+        }
+    }
+}
